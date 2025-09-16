173da60e29a0afec10fe7246d839b0a40f220a19
Ï589591 <bruno.osio@sap.com>
2025-09-16 20:03:40 -0300

    feat: add luis refeicaoservice

---
+++ api/v1/refeicao/RefeicaoController.php
@@ -0,0 +1,45 @@
+<?php
+      
+    require_once('./refeicaoService.php');
+    require_once('../../database/Banco.php');
+    
+    try {  
+        $jsonPostData = json_decode(file_get_contents("php://input"), true);
+
+        $operacao = isset($_REQUEST['operacao']) ? $_REQUEST['operacao'] : "Não informado [Erro]";
+    
+        $banco = new Banco(null,null,null,null,null,null);
+        
+        $refeicaoService = new RefeicaoService($banco);
+        
+        switch ($operacao) {
+            case 'getRefeicoes':
+                $refeicaoService->getRefeicoes();
+                break;   
+            
+            default:
+                $banco->setMensagem(1, 'Operação informada não tratada. Operação=' . $operacao);
+                break;
+        }
+
+        echo $banco->getRetorno();
+        unset($banco);
+    }
+    catch(Exception $e) {   
+        if (isset($banco)) {   
+            $banco->setMensagem(1, $e->getMessage());
+            echo $banco->getRetorno();
+            unset($banco);
+        } else {
+            header("Content-Type: application/json; charset=UTF-8");
+            echo json_encode([
+                "operacao" => isset($operacao) ? $operacao : 'desconhecida',
+                "NumMens" => 1,
+                "Mensagem" => $e->getMessage(),
+                "registros" => 0,
+                "dados" => null
+            ]);
+        }
+    }
+            
+?>
